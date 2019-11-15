6d334403cb7fd88cddbd919e6809edfc2ca4e308
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-11-15 19:01:18 +0900

    add Photo

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 const { ApolloServer } = require('apollo-server');
 // define schema
 const typeDefs = `
+  type Photo{
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
   }
   type Mutation {
-    postPhoto(name: String! description: String): Boolean!
+    postPhoto(name: String! description: String): Photo!
   }
 `;
+var _id = 0;
 var photos = [];
 
 // define resolver
 const resolvers = {
   Query: {
-    totalPhotos: () => photos.length
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
   },
   Mutation: {
     postPhoto(parent, args) {
+      var newPhoto = {
+        id: _id++,
+        ...args
+      };
       photos.push(args);
-      return true;
+      return newPhoto;
     }
+  },
+  Photo: {
+    url: parent => `http://locallhost:4000/img/${parent.id}.jpeg`
   }
 };
 
